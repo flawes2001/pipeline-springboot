2164370a13e8eca76963c2bb4de0fe4588b7dba8
flawes2001 <sousousi2001@gmail.com>
2025-04-20 15:15:23 +0100

    Initial commit

---
+++ WassimDemo1/src/test/java/com/example/WassimDemo1/WassimDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WassimDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WassimDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
